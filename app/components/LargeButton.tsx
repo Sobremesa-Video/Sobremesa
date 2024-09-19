8f301b58198307605874be803107af0846ab1dfb
monte <ariel.montmaj@gmail.com>
2024-09-19 12:38:37 -0400

    Changes CSS back to tailwind

--- app/components/LargeButton.tsx
+++ app/components/LargeButton.tsx
@@ -1,7 +1,7 @@
 // app/components/LargeButton.tsx
 
 import React from 'react';
-import styles from './LargeButton.module.css';
+import { Link } from '@remix-run/react';
 
 interface LargeButtonProps {
   href: string;
@@ -10,9 +10,9 @@ interface LargeButtonProps {
 
 const LargeButton: React.FC<LargeButtonProps> = ({ href, label }) => {
   return (
-    <a href={href} className={styles.button}>
+    <Link to={href} className="bg-white text-gray-800 border border-gray-300 rounded-lg px-6 py-3 shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 transition">
       {label}
-    </a>
+    </Link>
   );
 };
 
