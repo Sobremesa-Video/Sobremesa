abf63ea2b898cc4072e01a0f47ac8b581122c836
johnh03 <johnhuang0330@gmail.com>
2024-09-19 22:51:36 -0400

    Redid the Login, Sign Up, and Guest pages

--- app/routes/main.tsx
+++ app/routes/main.tsx
@@ -1,9 +1,9 @@
 export default function MainPage() {
-    return (
-      <div>
-        <h1>Welcome to Sombremesa</h1>
-        {/* The video player options will go here */}
-      </div>
-    );
-  }
-  
+  return (
+    <div className="flex flex-col justify-center items-center min-h-screen bg-white dark:bg-gray-950">
+      <h1 className="text-6xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-blue-600 to-purple-600 drop-shadow-lg mb-8">
+        Welcome to Sobremesa</h1>
+      {/* The video player options will go here */}
+    </div>
+  );
+}
