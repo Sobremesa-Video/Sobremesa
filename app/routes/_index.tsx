2fd8e19f306d4cbd5736c52e1bb57d14e5064de3
Ariel Montero <timelord1102@gmail.com>
2024-10-04 17:50:07 -0400

    Updates home page to darkmode style

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,5 +1,7 @@
 import type { MetaFunction } from "@remix-run/node";
-import LargeButton from "../components/LargeButton";
+import "app\\assets\\logo\\sobremesa.svg";
+import "app/styles/default.css";
+import { Link } from '@remix-run/react';
 
 export const meta: MetaFunction = () => {
   return [
@@ -10,39 +12,23 @@ export const meta: MetaFunction = () => {
 
 export default function Index() {
   return (
-    <div className="flex flex-col justify-center items-center min-h-screen bg-gradient-to-b from-gray-900 via-purple-900 to-black animate-gradient-x">
-      <h1
-        className="text-6xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-blue-300 to-purple-500 drop-shadow-glow mb-12 animate-colorFlow"
-        style={{ backgroundSize: '200% 200%' }}
-        >
-        Sobremesa
-      </h1>
-      <div className="flex flex-col gap-6 items-center">
-        <div className="flex flex-row gap-6">
-        {/* Button 1: Log In */}
-        <div>
-          <LargeButton
-            href="/login"
-            label="Log In"
-          />
+    <div className = "background">
+    <div className = "mainGrid">
+      <div className = "welcomeCenter">
+        <div className = "texture" />
+        <div className = "logo">
+          <img src = "app/assets/logo/sobremesa.svg" alt = "Sobremesa logo" />
         </div>
-        {/* Button 2: Sign Up */}
-        <div>
-          <LargeButton
-            href="/signup"
-            label="Sign Up"
-          />
+        <div className = "welcomeText">
+          <h1>Sobremesa</h1>
+          <p>A new way to movie night</p>
         </div>
-        </div>
-
-        {/* Guest Link */}
-        <a
-          href="/guest"
-          className="text-sm text-blue-400 hover:text-white transition-colors duration-300 mt-4"
-        >
-          No Thanks (Use as guest)
-        </a>
       </div>
+      <div className = "accountCenter">
+        <Link to="/login" className = 'loginButton'>Log in</Link>
+        <Link to="/signup" className = 'signupButton'>Sign up</Link>
+      </div>
+    </div>
     </div>
   );
 }
