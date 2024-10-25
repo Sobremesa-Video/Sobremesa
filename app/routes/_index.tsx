47e91b946a752d0195aa23d4f59e537b95e311b5
Ariel Montero <timelord1102@gmail.com>
2024-10-25 16:29:28 -0400

    Adds key combo (ctrl+m) to get to video player page

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import type { MetaFunction } from "@remix-run/node";
 import "app\\assets\\logo\\sobremesa.svg";
 import "app/styles/default.css";
@@ -12,6 +13,19 @@ export const meta: MetaFunction = () => {
 };
 
 export default function Index() {
+  useEffect(() => {
+    const handleKeyDown = (event: KeyboardEvent) => {
+      if (event.ctrlKey && event.key === 'm') {
+        window.location.href = '/main';
+      }
+    };
+
+    window.addEventListener('keydown', handleKeyDown);
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+  }, []);
+
   return (
     <div className="background">
       <div className="mainGrid">
